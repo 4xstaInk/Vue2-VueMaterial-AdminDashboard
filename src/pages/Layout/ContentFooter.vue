0c698856910c9c20020d12363f2d1c71c1d87725
4xstaInk <45216915+4xstaInk@users.noreply.github.com>
2021-05-25 12:50:07 +0200

    initial commit

---
+++ src/pages/Layout/ContentFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href="">Pareza Group</a>
+          </li>
+          <li>
+            <a href="">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Blog
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center text-primary">
+        &copy; {{ new Date().getFullYear() }}
+        <a href="" target="_blank"
+          >Pareza Groups</a
+        >
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
